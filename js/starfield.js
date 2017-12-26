81a227998fb9724c89f49619dbd1ac410a15df3b
Joseph Finlayson <joseph.finlayson@gmail.com>
2017-12-26 22:37:08 +0100

    resize canvas on window change

--- js/starfield.js
+++ js/starfield.js
@@ -83,3 +83,21 @@ if (document.readyState === 'complete') {
     ngn.setup();
   };
 }
+
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+
+window.addEventListener('resize', debounce(ngn.setup, 200))
